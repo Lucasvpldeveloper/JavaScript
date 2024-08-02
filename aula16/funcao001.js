144933d9bf01870d4aa18aa03515c85c4221301a
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-08-01 23:52:13 -0300

    Ex funcao01/02/03.js

---
+++ aula16/funcao001.js
@@ -0,0 +1,8 @@
+function parimpar(n /*Parametro formal*/) {
+    if (n % 2 == 0) {
+        return 'Par'
+    } else {
+        return 'ímpar'
+    }
+}
+console.log(/*Chamada*/parimpar(/*Parametro real / vai ser jogado no parametro formal*/4))
