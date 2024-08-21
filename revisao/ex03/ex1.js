d763816fe8d18a2d48b310bbbb947bece79a3d8d
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-08-21 14:10:28 -0300

    Praticando JS

---
+++ revisao/ex03/ex1.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}`)
+    if (vel > 60) { // condição simples
+        console.log(`Sua velocidade atual é ${vel}Km/h você foi multado por ultrapassar a velocidade permitida!`)
+    } 
+console.log('Use sempre sinto de segurança!!!')
