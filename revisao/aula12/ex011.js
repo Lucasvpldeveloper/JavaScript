8ec6836a832986481272db156bcf3cbfe77444f4
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-08-27 21:39:57 -0300

    Praticando JS

---
+++ revisao/aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 65
+if (idade < 16) {
+    console.log(`Não vota`)
+} else if (idade >= 16 && idade < 18 || idade > 65) {
+    console.log('Voto opcional')
+} else if (idade >= 18 && idade < 66) {
+    console.log('Voto obrigatório')
+} else {
+    console.log('Repita sua idade')
+}
