931583f603a00482c702aa6dd9ab193bf8d4b5c3
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-09-07 21:38:10 -0300

    Praticando.js

--- revisao/ex06/verificador.js
+++ revisao/ex06/verificador.js
@@ -1,47 +1,50 @@
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var anonascimento = document.getElementById('anonasc') 
+    var ano = new Date()
+    var anoatual = ano.getFullYear()
+    var anonascimento = document.getElementById('anonasc')
     var res = document.getElementById('res')
-    if (anonascimento.value.length == 0 || anonascimento.value > ano) {
-        alert('[ERRO] Por favor repita os dados informados!')
+    if (anonascimento.value.length == 0 || anonascimento.value > anoatual) {
+        alert('[ERRO] Repita o ano informado novamente!')
     } else {
         var fsex = document.getElementsByName('sexo')
-        var idade = ano - Number(anonascimento.value)
-        var genero = ''
+        var idade = anoatual - Number(anonascimento.value)
         var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
+        img.setAttribute('id','foto')
+        var genero = ''
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
-                // criança
+                //Criança
                 img.setAttribute('src','menino.png')
-            } else if (idade < 21) {
-                // Adolecente
+            } else if (idade < 20) {
+                //Adolecente
                 img.setAttribute('src','homem-jovem.png')
             } else if (idade < 50) {
-                // Idoso
+                //Adulto
                 img.setAttribute('src','homem-adulto.png')
             } else {
+                //idoso
                 img.setAttribute('src','homem-velho.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
-                // criança
+                //Criança
                 img.setAttribute('src','menina.png')
-            } else if (idade < 21) {
-                // Adolecente
+            } else if (idade < 20) {
+                //Adolecente
                 img.setAttribute('src','mulher-jovem10.png')
             } else if (idade < 50) {
-                // Idoso
+                //Adulto
                 img.setAttribute('src','mulher-adulta.png')
             } else {
+                //idoso
                 img.setAttribute('src','mulher-velha.png')
             }
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
     }
+    
+    res.innerHTML = `Você é ${genero} e a sua idade atual é ${idade}`
+    res.style.textAlign = 'center'
+    res.appendChild(img)
 }
