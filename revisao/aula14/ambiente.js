bae1729d3906fc766ebd48f97b7912557bd67bab
Lucas Vinicius Portela Leite <lucasvpldeveloper@gmail.com>
2024-09-22 19:00:47 -0300

    Praticando js

--- revisao/aula14/ambiente.js
+++ revisao/aula14/ambiente.js
@@ -1,3 +1,5 @@
-for (var c = 1; c >= 1; c++) {
+console.log('Vai começar...')
+for (var c = 1; c <= 1; c++) {
     console.log('Tudo bem?')
 }
+console.log('FIM!')
--- revisao/aula14/node_9c9047d0a80a1.tmp.js
+++ revisao/aula14/node_9c9047d0a80a1.tmp.js
@@ -1,3 +1,5 @@
+console.log('Vai começar...')
 for (var c = 1; c >= 1; c++) {
     console.log('Tudo bem?')
 }
+console.log('FIM!')
